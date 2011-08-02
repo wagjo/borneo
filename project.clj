a4e76c2f860d702856163a87f2ce9e1c98ba7081
Jozef Wagner <jozef.wagner@gmail.com>
2011-08-02 11:00:03 +0200

    supporting neo4j 1.4 and 1.2.1 clojure

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject borneo "0.2.0-SNAPSHOT" 
+(defproject borneo "0.3.0-SNAPSHOT" 
   :description "Clojure wrapper for Neo4j, a graph database."
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.neo4j/neo4j "1.2"]]
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [org.neo4j/neo4j "1.4"]]
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]
                      [lein-clojars "0.6.0"]
                      [org.clojars.rayne/autodoc "0.8.0-SNAPSHOT"]]
