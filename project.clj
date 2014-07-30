a01ea7cd18e81e81c5922a50596ef0867e836271
Jozef Wagner <jozef.wagner@gmail.com>
2014-07-30 10:20:58 +0200

    codox deps

--- project.clj
+++ project.clj
@@ -6,13 +6,9 @@
   :dev-dependencies [[org.clojars.rayne/autodoc "0.8.0-SNAPSHOT"]]
   :profiles {:dev {:dependencies [[midje "1.6.3"]]
                    :plugins [[lein-midje "3.1.3"]]}}
-  :jvm-opts ["-Dswank.encoding=utf-8"]
+  :plugins [[codox "0.8.10"]]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :autodoc {:copyright "Copyright (C) 2011, Jozef Wagner. All rights reserved."
+  :autodoc {:copyright "Copyright (C) 2011, 2014, Jozef Wagner. All rights reserved."
             :web-src-dir "http://github.com/wagjo/borneo/blob/"
             :web-home "http://wagjo.github.com/borneo/"})
-
-;;; you should install swank-clojure and lein-clojars locally:
-;; lein plugin install swank-clojure 1.3.2
-;; lein plugin install lein-clojars  0.7.0
